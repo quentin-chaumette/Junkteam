9bde5fad87f888fbe8a6a40bf692bec8d11895c2
Oscar Deloizy <osc.deloizy@gmail.com>
2015-06-17 19:39:09 +0200

    front stuff before pull

--- JunkTime/AddCategoryController.swift
+++ JunkTime/AddCategoryController.swift
@@ -9,67 +9,74 @@
 import UIKit
 
 class AddCategoryController: UIViewController {
-	
-	@IBOutlet weak var titleTextField: UITextField! = UITextField()
-
-	
-	
-	override func viewDidLoad() {
-		super.viewDidLoad()
-		var btn = UIBarButtonItem(title: "Back", style: .Plain, target: self, action: "backBtnClicked")
-		self.navigationController?.navigationBar.topItem?.backBarButtonItem=btn
-		// Do any additional setup after loading the view.
-	}
-	
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
-	}
-	
-	@IBAction func addButtonTapped(sender: AnyObject) {
-		var userDefaults:NSUserDefaults = NSUserDefaults.standardUserDefaults()
-		var categoryList:NSMutableArray? = userDefaults.objectForKey("categoryList") as? NSMutableArray
-		var dataSet:NSMutableDictionary = NSMutableDictionary()
-		let date = NSDate()
-		var categoryTime = "00:00:00"
-		
-		dataSet.setObject(titleTextField.text, forKey: "categoryTitle")
-		dataSet.setObject(date, forKey: "dateOfCatCreation")
-		dataSet.setObject(categoryTime, forKey: "categoryTime")
-
-		if ((categoryList) != nil){	  // data already available
-			var newMutableList:NSMutableArray = NSMutableArray()
-			for dict:AnyObject in categoryList!{
-				newMutableList.addObject(dict as! NSDictionary)
-			}
-			userDefaults.removeObjectForKey("categoryList")
-			newMutableList.addObject(dataSet)
-			userDefaults.setObject(newMutableList, forKey: "categoryList")
-			
-		}
-		else{			   // this is the first todo item in the list
-			userDefaults.removeObjectForKey("categoryList")
-			categoryList = NSMutableArray()
-			categoryList!.addObject(dataSet)
-			userDefaults.setObject(categoryList, forKey: "categoryList")
-			
-		}
-		
-		userDefaults.synchronize()
-		
-		self.navigationController!.popToRootViewControllerAnimated(true)
-		
-		
-	}
-	
-	/*
-	// MARK: - Navigation
-	
-	// In a storyboard-based application, you will often want to do a little preparation before navigation
-	override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-	// Get the new view controller using segue.destinationViewController.
-	// Pass the selected object to the new view controller.
-	}
-	*/
-	
+    
+    @IBOutlet weak var titleTextField: UITextField! = UITextField()
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        var btn = UIBarButtonItem(title: "Back", style: .Plain, target: self, action: "backBtnClicked")
+        self.navigationController?.navigationBar.topItem?.backBarButtonItem=btn
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func addButtonTapped(sender: AnyObject) {
+        
+        if (titleTextField.text.isEmpty) {
+            
+        } else {
+            
+            var userDefaults:NSUserDefaults = NSUserDefaults.standardUserDefaults()
+            var categoryList:NSMutableArray? = userDefaults.objectForKey("categoryList") as? NSMutableArray
+            var dataSet:NSMutableDictionary = NSMutableDictionary()
+            let date = NSDate()
+            var categoryTime = "00:00:00"
+            
+            dataSet.setObject(titleTextField.text, forKey: "categoryTitle")
+            dataSet.setObject(date, forKey: "dateOfCatCreation")
+            dataSet.setObject(categoryTime, forKey: "categoryTime")
+            
+            if ((categoryList) != nil){	  // data already available
+                var newMutableList:NSMutableArray = NSMutableArray()
+                for dict:AnyObject in categoryList!{
+                    newMutableList.addObject(dict as! NSDictionary)
+                }
+                userDefaults.removeObjectForKey("categoryList")
+                newMutableList.addObject(dataSet)
+                userDefaults.setObject(newMutableList, forKey: "categoryList")
+                
+            }
+            else {			   // this is the first todo item in the list
+                userDefaults.removeObjectForKey("categoryList")
+                categoryList = NSMutableArray()
+                categoryList!.addObject(dataSet)
+                userDefaults.setObject(categoryList, forKey: "categoryList")
+                
+            }
+            
+            userDefaults.synchronize()
+            
+            self.navigationController!.popToRootViewControllerAnimated(true)
+            
+        }
+        
+        
+    }
+    
+    /*
+    // MARK: - Navigation
+    
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
+    }
+    */
+    
 }
--- JunkTime/AddTaskController.swift
+++ JunkTime/AddTaskController.swift
@@ -9,75 +9,83 @@
 import UIKit
 
 class AddTaskController: UIViewController {
-	
-	@IBOutlet var taskTitle: UITextField! = UITextField()
-	
-	var CategoryContainer:String = String()
-	
-	
-	
-	override func viewDidLoad() {
-		super.viewDidLoad()
-		var btn = UIBarButtonItem(title: "Back", style: .Plain, target: self, action: "backBtnClicked")
-		self.navigationController?.navigationBar.topItem?.backBarButtonItem=btn
-	}
-	
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-	}
-	
-	
-	
-	@IBAction func AddTaskButton(sender: AnyObject) {	
-		var userDefaults:NSUserDefaults = NSUserDefaults.standardUserDefaults()
-		var tasksList:NSMutableArray? = userDefaults.objectForKey("tasksList") as? NSMutableArray
-		var dataSet:NSMutableDictionary = NSMutableDictionary()
-		let date = NSDate()
-		var taskTime = "00:00:00"
-		
-		dataSet.setObject(taskTitle.text, forKey: "taskTitle")
-		dataSet.setObject(date, forKey: "dateOfTaskCreation")
-		dataSet.setObject(CategoryContainer, forKey: "categoryOfThisTask")
-		dataSet.setObject(taskTime, forKey: "taskTime")
-		
-				
-		if ((tasksList) != nil){	  // data already available
-			var newMutableList:NSMutableArray = NSMutableArray()
-			for dict:AnyObject in tasksList!{
-				newMutableList.addObject(dict as! NSDictionary)
-			}
-			userDefaults.removeObjectForKey("tasksList")
-			newMutableList.addObject(dataSet)
-			userDefaults.setObject(newMutableList, forKey: "tasksList")
-		}
-		else{			   // this is the first todo item in the list
-			
-			userDefaults.removeObjectForKey("tasksList")
-			tasksList = NSMutableArray()
-			tasksList!.addObject(dataSet)
-			userDefaults.setObject(tasksList, forKey: "tasksList")			
-		}
-		
-		userDefaults.synchronize()
-		
-		//self.navigationController!.popToViewController(WorkTaskViewController(), animated: true)
-		self.navigationController!.popViewControllerAnimated(true)
-		
-	}
-	
-	
-	
-	
-	
-	
-	/*
-	// MARK: - Navigation
-	
-	// In a storyboard-based application, you will often want to do a little preparation before navigation
-	override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-	// Get the new view controller using segue.destinationViewController.
-	// Pass the selected object to the new view controller.
-	}
-	*/
-	
+    
+    @IBOutlet var taskTitle: UITextField! = UITextField()
+    
+    var CategoryContainer:String = String()
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        var btn = UIBarButtonItem(title: "Back", style: .Plain, target: self, action: "backBtnClicked")
+        self.navigationController?.navigationBar.topItem?.backBarButtonItem=btn
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    
+    
+    @IBAction func AddTaskButton(sender: AnyObject) {
+        
+        if (taskTitle.text.isEmpty) {
+            
+        } else {
+            
+            
+            var userDefaults:NSUserDefaults = NSUserDefaults.standardUserDefaults()
+            var tasksList:NSMutableArray? = userDefaults.objectForKey("tasksList") as? NSMutableArray
+            var dataSet:NSMutableDictionary = NSMutableDictionary()
+            let date = NSDate()
+            var taskTime = "00:00:00"
+            
+            dataSet.setObject(taskTitle.text, forKey: "taskTitle")
+            dataSet.setObject(date, forKey: "dateOfTaskCreation")
+            dataSet.setObject(CategoryContainer, forKey: "categoryOfThisTask")
+            dataSet.setObject(taskTime, forKey: "taskTime")
+            
+            
+            if ((tasksList) != nil){	  // data already available
+                var newMutableList:NSMutableArray = NSMutableArray()
+                for dict:AnyObject in tasksList!{
+                    newMutableList.addObject(dict as! NSDictionary)
+                }
+                userDefaults.removeObjectForKey("tasksList")
+                newMutableList.addObject(dataSet)
+                userDefaults.setObject(newMutableList, forKey: "tasksList")
+            }
+            else{			   // this is the first todo item in the list
+                
+                userDefaults.removeObjectForKey("tasksList")
+                tasksList = NSMutableArray()
+                tasksList!.addObject(dataSet)
+                userDefaults.setObject(tasksList, forKey: "tasksList")
+            }
+            
+            userDefaults.synchronize()
+            
+            //self.navigationController!.popToViewController(WorkTaskViewController(), animated: true)
+            self.navigationController!.popViewControllerAnimated(true)
+            
+        }
+        
+    }
+    
+    
+    
+    
+    
+    
+    /*
+    // MARK: - Navigation
+    
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
+    }
+    */
+    
 }
